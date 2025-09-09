b045d25dfd3e02ea1fa6968bcbc8d9e12d805711
Anagha <118123726+anaghabbu@users.noreply.github.com>
2025-09-09 15:51:50 -0400

    Update newton_solver.m

--- newton_solver.m
+++ newton_solver.m
@@ -1,15 +1,19 @@
 function xroot = newton_solver(fun, xn, f_tol, x_tol, max_iter)
 
+    dx = 2 *x_tol;
     count = 0;
+        
+    [fval, dfdx] = fun(xn);
 
-    Fn = fun(xn);
+    %Fn = fun(xn);
 
         %guesses = xn
-        while abs(Fn) > f_tol 
-
+        while count<max_iter && abs(fval) > f_tol && abs(dx) > x_tol
+            
+            dx = -fval/dfdx;
             count = count + 1;
 
-            xn = xn - fun(xn)/derfun(xn);
+            xn = xn - fval/dfdx;
             % guesses (end + 1) = xn
         end 
         
