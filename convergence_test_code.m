78d2325bacfc9c742c8f9c05e12718677fd43f77
Anagha <118123726+anaghabbu@users.noreply.github.com>
2025-09-10 22:41:54 -0400

    sad figuring out step eight

--- convergence_test_code.m
+++ convergence_test_code.m
@@ -1,9 +1,18 @@
-solver_flag = 1
-fun = @test_func01
-x_guess0 = 1;
-guess_list1 = 1;
-guess_list2 = 0;
-filter_list
+function convergence_test_code()
 
-function convergence_analysis(solver_flag, fun, ...
-x_guess0, guess_list1, guess_list2, filter_list)
+    solver_flag = 1;
+    fun = @test_func01;
+    x_guess0 = linspace(-5, .5, 100);
+    guess_list1 = linspace(1, 6.5, 100);
+    guess_list2 = linspace(1, 6.5, 100);
+    filter_list1 = 1e-15;
+    filter_list2 = 1e-2;
+    filter_list3 = 1e-14;
+    filter_list4 = 1e-2;
+    filter_list5 = 2;
+
+    convergence_analysis(solver_flag, fun, ...
+    x_guess0, guess_list1, filter_list1, ...
+    filter_list2, filter_list3, filter_list4, filter_list5)
+
+end
